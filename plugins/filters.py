06ad39a346da9e5bf3df973efdac8adf55ccb916
oliverlion8 <80013576+oliverlion8@users.noreply.github.com>
2021-03-04 21:47:37 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -158,9 +158,7 @@ async def cb_handler(client: Bot, query:CallbackQuery):
         elif query.data == "start_data":
 
             keyboard = InlineKeyboardMarkup([
-                [InlineKeyboardButton("HELP", callback_data="help_data"),
-                    InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")]
+                [InlineKeyboardButton("👑 ᴄᴏɴᴛᴀᴄᴛ ᴍʏ ᴋɪɴɢ 👑", url="https://t.me/TroJanzHEX")]
             ])
 
             await query.message.edit_text(
